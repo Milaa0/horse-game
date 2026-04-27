0697e4661773c27e82a625028d4005d09a4d24f9
Milaa0 <milenakuzminska0@gmail.com>
2026-04-28 01:48:08 +0200

    Add initial JavaScript file structure and main.js

---
+++ T-HorseGame/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿import { Game } from './Game.js ';
+
+window.addEventListener('load', () => {
+    const tHorseGame = new Game();
+    tHorseGame.start();
+
+    window.addEventListener('keydown, (e) => {
+       if (e.code === 'Space') {
+        tHorseGame.horse.jump();
+        }
+    });
+});
